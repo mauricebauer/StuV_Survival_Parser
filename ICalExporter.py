37485805a65bd7e2d212a0262f0d9b091972a336
Maurice Bauer <mp.bauer@freenet.de>
2018-10-17 11:28:48 +0200

    Added necessary fields for Ical regarding to the standards

--- ICalExporter.py
+++ ICalExporter.py
@@ -3,23 +3,28 @@ from datetime import datetime
 from icalendar import Calendar, Event
 import dateutil.parser
 
-# lectures is a list of lecture objects
-
 
 class ICalExporter:
+    # lectures is a list of lecture objects
     def __init__(self, lectures):
         self.lectures = lectures
 
     def exportICS(self, filename: str):
         cal = Calendar()
+        cal.add('version', "2.0")
+        cal.add('prodid', "StuV_Survival_Parser")
+        uid = 0
         for lec in self.lectures:
             event = Event()
+            event.add('uid', str(uid))
             event.add('summary', lec.name)
             event.add('dtstart', dateutil.parser.parse(lec.startTime))
             event.add('dtend', dateutil.parser.parse(lec.endTime))
+            event.add('dtstamp', datetime.now())
             event.add('location', lec.room)
             event.add('description', "Dozent: " + lec.lecturer)
             cal.add_component(event)
+            uid += 1    # Increase the universal id of each event
         f = open(filename, 'wb')
         f.write(cal.to_ical())
         f.close()
